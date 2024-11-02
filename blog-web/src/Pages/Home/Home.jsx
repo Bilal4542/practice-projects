b66d9ed54b9477896f17230d53fb4402c70ccc34
Muhammad Bilal <mbilal45422@gmail.com>
2024-11-02 18:52:29 +0500

    working on home page

--- blog-web/src/Pages/Home/Home.jsx
+++ blog-web/src/Pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Banner from './Banner'
+import PostCards from '../Blogs/PostCards'
+import Category from './Category'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='mt-10'>
+      <Banner/>
+      <div className='block lg:flex lg:space-x-2 px-2 lg:p-0 mt-10 mb-10'>
+      <PostCards/>
+      <Category/>
+      </div>
     </div>
   )
 }
