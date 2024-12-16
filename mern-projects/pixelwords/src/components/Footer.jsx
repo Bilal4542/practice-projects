1dc7c368824c160f9f83d9fc99399213f5479382
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2024-12-16 09:56:42 +0500

    result page completed

--- mern-projects/pixelwords/src/components/Footer.jsx
+++ mern-projects/pixelwords/src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='flex items-center justify-between gap-4 py-3 mt-20'>
+      <img src={logo} width={50} alt="" />
+      <p className='flex-1 border-l border-gray-400 text-gray-500 pl-4 text-sm max-sm:hidden'>Copyright @PixelWords.com | All rights reserved.</p>
+
+      <div className="flex gap-2.5">
+      <FaFacebookSquare size={20}/>
+      <FaTwitterSquare size={20}/>
+      <FaSquareInstagram size={20}/>
+      </div>
     </div>
   )
 }
--- mern-projects/pixelwords/src/pages/Result.jsx
+++ mern-projects/pixelwords/src/pages/Result.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Result = () => {
+
+  const [image, setImage] = useState('https://cdn.pixabay.com/photo/2024/08/29/12/36/images-9006863_1280.jpg')
+
+  const [isImageLoaded, setIsImageLoaded] = useState(false)
+
+  const [loading, setLoading] = useState(false)
+
+  const [input, setInput] = useState('')
+
+  const onSubmitHandler =  async (e) => {
+
+  }
+
   return (
-    <div>
-      Result Page
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center justify-center min-h-[90vh]'>
+    <div className=''>
+      <div className="relative">
+        <img src={image} className='max-w-sm rounded' alt="" />
+        <span className={`absolute h-1 bg-blue-500 bottom-0 left-0 ${loading ? 'w-full transition-all duration-[10s]' : 'w-0'} `}/>
+      </div>
+      <p className={!loading ? 'hidden' : ''}>Loading....</p>
     </div>
+    {!isImageLoaded &&
+    <div className="flex max-w-xl w-full bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
+      <input onChange={e => setInput(e.target.value)} value={input} type="text" placeholder='Describe What you want to Generate' className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20' />
+      <button type='submit' className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'>Generate</button>
+    </div>
+    }
+
+    {isImageLoaded &&
+    <div className="flex flex-wrap gap-2 justify-center text-white text-sm p-0.5 mt-10 rounded-full">
+      <p onClick={()=>{setIsImageLoaded(false)}} className='bg-transparent border border-zinc-900 text-black py-3 px-8 rounded-full cursor-pointer'>Generate Another</p>
+      <a href={image} className='bg-zinc-900 py-3 px-8 rounded-full cursor-pointer'>Download</a>
+    </div>
+    }
+    </form>
   )
 }
 
