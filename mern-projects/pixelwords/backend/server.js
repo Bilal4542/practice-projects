3f40d75145c40587033f5b7a67b3ef09202902e8
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2024-12-21 10:33:29 +0500

    working on controllers

--- mern-projects/pixelwords/backend/server.js
+++ mern-projects/pixelwords/backend/server.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
 
 const PORT = process.env.PORT || 4000
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+await connectDB();
 
-app.get('/', (req, res)=> res.send('API is Working Successfully'))
+app.get('/', (req, res)=> res.send('API is Working Successfullys'))
 
 app.listen(PORT, ()=> console.log('Server is Running on port ' + PORT))
