bf949825471b63347b262f4ad084a8e80d58b51c
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2024-12-23 11:37:43 +0500

    controllers

--- mern-projects/pixelwords/backend/controllers/userController.js
+++ mern-projects/pixelwords/backend/controllers/userController.js
@@ -2,7 +2,7 @@ import userModel from "../models/userModel.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
-const registerUser = async (req, res) => {
+export const registerUser = async (req, res) => {
     try {
         const {name, email, password} = req.body;
         if(!name || !email || !password){
@@ -25,6 +25,38 @@ const registerUser = async (req, res) => {
 
         res.json({success: true, token, user: {name:user.name}})
 
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const loginUser = async (req,res) => {
+    try {
+        const {email,password} = req.body
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.json({success:false, message:"user does not exists..."})
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (isMatch) {
+            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+
+            res.json({success: true, token, user: {name:user.name}})
+        }else{
+            return res.json({success:false, message:"Invalid Credentials..."})
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }   
+}
+
+export const userCredits = async (req,res) => {
+    try {
+        const {userId} = req.body
+        const user = await userModel.findById(userId)
+        res.json({success:true, credits: user.creditbalance, user:{name:user.name}})
     } catch (error) {
         console.log(error)
         res.json({success: false, message: error.message})
