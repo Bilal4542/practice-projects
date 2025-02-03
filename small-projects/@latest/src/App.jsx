a240a9e8c70c2ec74a03a34fd36c11ee7215fda3
Muhammad Bilal <114822973+Bilal4542@users.noreply.github.com>
2025-02-03 19:11:17 +0500

    todo

---
+++ small-projects/@latest/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+function App() {
+  const [task, setTask] = useState('')
+  const [data, setData] = useState([])
+
+  const addTask = (e) => {
+    e.preventDefault()
+    if(task.trim()){
+      setData([...data, {id: Date.now(), name:task, completed:false}])
+      setTask('')
+      console.log(data)
+    }
+  }
+
+  const toggleTask = (id) => {
+    setData(data.map((t)=> t.id === id ? {...t, completed : !t.completed } : t))
+  }
+
+  return (
+    <>
+      <h1>Todo List</h1>
+      <form onSubmit={addTask}>
+        <input placeholder='Enter Your ToDo...' type="text" value={task} onChange={(e)=>setTask(e.target.value)}  />
+        <button type='submit'>Add Task</button>
+      </form>
+      <ul>
+        {data.map((t)=>(
+          <li key={t.id}>{t.name}
+          <button onClick={toggleTask(t.id)}>{t.completed ? 'Undo' : 'Completed'}</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
